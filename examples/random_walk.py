d926b2cf5b4d83ad751475543b82f1136e0e41b6
Jacques Kvam <jacques@verdigris.co>
2016-08-27 23:12:48 -0700

    added examples dir with random walk

---
+++ examples/random_walk.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bowtie.control import Nouislider
+from bowtie.visual import Plotly
+
+import numpy as np
+from numpy import random as rng
+import plotlywrapper as pw
+
+
+sigma = Nouislider(caption='Sigma', start=1, minimum=0.1, maximum=50)
+mainplot = Plotly()
+
+data = np.zeros(100).tolist()
+
+
+def walk():
+    value = float(sigma.get())
+    data.pop(0)
+    data.append(value * rng.randn() + data[-1])
+    mainplot.do_all(pw.line(data).to_json())
+
+
+if __name__ == "__main__":
+    from bowtie import Layout
+    layout = Layout()
+    layout.add_controller(sigma)
+    layout.add_visual(mainplot)
+    layout.schedule(0.1, walk)
+
+    layout.build(debug=False)
