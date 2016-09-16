2715ee5e6f7e52523395a3aea6ba9af7f29b7f89
Jacques Kvam <jacques@verdigris.co>
2016-09-16 11:26:27 -0700

    remove consolelog from button

--- bowtie/__init__.py
+++ bowtie/__init__.py
@@ -2,7 +2,7 @@
 Bowtie
 """
 
-__version__ = '0.0.7'
+__version__ = '0.0.8-dev'
 
 from flask_socketio import emit
 from bowtie._layout import Layout
--- bowtie/templates/button.jsx
+++ bowtie/templates/button.jsx
@@ -10,14 +10,10 @@ export default class SimpleButton extends React.Component {
     }
 
     handleClick(event) {
-        console.log('clicked');
         this.props.socket.emit(this.props.uuid + '#click');
     }
 
     render() {
-        // var hw = get_height_width();
-        // var height = Math.floor(hw[1] / this.rops.rows);
-        // var width = Math.floor((hw[0] * 9 / 10) / this.props.columns);
         return (
             <Button 
                 label={this.props.label} 
