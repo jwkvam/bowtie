644a2a93e11db6850f71445f1b1218385bec50e3
Jacques Kvam <jwkvam@gmail.com>
2017-02-14 11:57:40 -0800

    fix argument counting in python 2 (#89)

--- bowtie/__init__.py
+++ bowtie/__init__.py
@@ -2,7 +2,7 @@
 Interactive dashboard toolkit
 """
 
-__version__ = '0.2.1'
+__version__ = '0.2.2-dev'
 
 from bowtie._layout import Layout
 from bowtie._command import command
---
+++ bowtie/tests/test_compat.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Compat testing
+"""
+
+from bowtie._compat import numargs
+
+
+def test_numargs():
+    """test numargs"""
+
+    # pylint: disable=missing-docstring
+    def onearg(x):
+        return x
+
+    def zeroarg():
+        pass
+
+    def varargs(*y):
+        return y
+
+    def onevarargs(x, *y):
+        return x, y
+
+    assert numargs(onearg) == 1
+    assert numargs(onevarargs) == 2
+    assert numargs(zeroarg) == 0
+    assert numargs(varargs) == 1
