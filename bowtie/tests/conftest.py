dc62fbd5af2196d4c33832fa25bc76efe5fd5b51
Jacques Kvam <jwkvam@gmail.com>
2017-05-31 08:34:44 -0700

    use chromedriver more tests (#133)
    * use chromedriver more tests
    
    * docstring
    
    * chromedriver to path
    
    * for macos
    
    * linux binary path for chrome
    
    * see where chrome is
    
    * update
    
    * update
    
    * update
    
    * try trusty
    
    * update chrome binary for linux
    
    * quit driver on finish
    
    * sleep again
    
    * lint
    
    * don't print
    
    * remove prints
    
    * remove import

--- bowtie/tests/conftest.py
+++ bowtie/tests/conftest.py
@@ -3,7 +3,9 @@
 """Pytest configuration."""
 
 import os
+from sys import platform
 import shutil
+from selenium import webdriver
 
 import pytest
 
@@ -14,3 +16,18 @@ def remove_build():
     yield
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'build')
     shutil.rmtree(path)
+
+
+@pytest.fixture
+def chrome_driver():
+    """Set up chrome driver."""
+    options = webdriver.ChromeOptions()
+    if platform == 'darwin':
+        options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    else:
+        options.binary_location = '/usr/bin/google-chrome-stable'
+    options.add_argument('headless')
+    options.add_argument('window-size=1200x800')
+    driver = webdriver.Chrome(chrome_options=options)
+    yield driver
+    driver.quit()
--- bowtie/tests/test_plotly.py
+++ bowtie/tests/test_plotly.py
@@ -6,27 +6,40 @@ import os
 import subprocess
 import time
 
-from selenium.webdriver import PhantomJS
+# from selenium.webdriver import PhantomJS
 # from selenium.webdriver import ActionChains
 
 from bowtie import Layout
 from bowtie.control import Nouislider, Button
 from bowtie.visual import Plotly
 
+from plotly.graph_objs import Scatter
+from plotly.graph_objs import Layout as PlotLayout
+
+
+# pylint: disable=invalid-name
+viz = Plotly()
+ctrl = Nouislider()
+ctrl2 = Button()
 
 
 def callback(*args):
     """dummy function"""
     # pylint: disable=unused-argument
-    pass
+    chart = {
+        "data": [
+            Scatter(x=[1, 2, 3, 4], y=[4, 1, 3, 7])
+        ],
+        "layout": PlotLayout(
+            title="hello world"
+        )
+    }
+    viz.do_all(chart)
 
 
 # pylint: disable=unused-argument
-def test_plotly(remove_build):
+def test_plotly(remove_build, chrome_driver):
     """Tests plotly."""
-    viz = Plotly()
-    ctrl = Nouislider()
-    ctrl2 = Button()
 
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'build')
     layout = Layout(directory=path)
@@ -40,11 +53,19 @@ def test_plotly(remove_build):
     env = os.environ
     env['PYTHONPATH'] = '{}:{}'.format(os.getcwd(), os.environ.get('PYTHONPATH', ''))
     server = subprocess.Popen(os.path.join(path, 'src/server.py'), env=env)
+
     time.sleep(5)
 
-    driver = PhantomJS()
-    driver.get('http://localhost:9991')
+    chrome_driver.get('http://localhost:9991')
+    chrome_driver.implicitly_wait(5)
 
-    assert driver.title == 'Bowtie App'
+    assert chrome_driver.title == 'Bowtie App'
+
+    button = chrome_driver.find_element_by_class_name('ant-btn')
+    button.click()
+
+    points = chrome_driver.find_elements_by_class_name('point')
+
+    assert len(points) == 4
 
     server.kill()
