d31bd80deb910076ecdbafba60e6f29025a594d1
Jacques Kvam <jwkvam@gmail.com>
2018-02-21 19:24:14 -0800

    fix tag test

--- bowtie/tests/test_tags.py
+++ bowtie/tests/test_tags.py
@@ -9,16 +9,14 @@ def test_markdown():
     # pylint: disable=protected-access
     next_uuid = Markdown._NEXT_UUID
     mark = Markdown()
-    assert mark._instantiate == ("<AntProgress socket={{socket}} uuid={{'{next_uuid}'}}>"
-                                 "<Markdown initial={{''}} "
-                                 "socket={{socket}} uuid={{'{next_uuid1}'}} />"
-                                 "</AntProgress>").format(next_uuid=next_uuid + 1,
-                                                          next_uuid1=next_uuid + 2)
+    assert mark._instantiate == (
+        "<Markdown initial={{''}} "
+        "socket={{socket}} uuid={{'{next_uuid}'}} />"
+    ).format(next_uuid=next_uuid + 1)
 
     next_uuid = Markdown._NEXT_UUID
     mark = Markdown(initial='#hi\n##hello')
-    assert mark._instantiate == ("<AntProgress socket={{socket}} uuid={{'{next_uuid}'}}>"
-                                 "<Markdown initial={{'<h1>hi</h1>\\n<h2>hello</h2>'}} "
-                                 "socket={{socket}} uuid={{'{next_uuid1}'}} />"
-                                 "</AntProgress>").format(next_uuid=next_uuid + 1,
-                                                          next_uuid1=next_uuid + 2)
+    assert mark._instantiate == (
+        "<Markdown initial={{'<h1>hi</h1>\\n<h2>hello</h2>'}} "
+        "socket={{socket}} uuid={{'{next_uuid}'}} />"
+    ).format(next_uuid=next_uuid + 1)
