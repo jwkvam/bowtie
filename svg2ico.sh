84c9c8fa3d03c9f84cf24d79ba96e4abf62a883c
Jacques Kvam <jwkvam@gmail.com>
2018-05-18 00:39:54 -0700

    remove files

--- svg2ico.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#convert -density 256x256 -background transparent bowtie.svg -define icon:auto-resize -colors 256 favicon.ico
-
-mogrify -format ico -density 600 -define icon:auto-resize=128,64,48,32,16 bowtie.svg
-
-
-
