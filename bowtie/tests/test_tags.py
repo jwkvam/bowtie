c7d2822f142a5508a12f069e91d3d98227068eb7
Jacques Kvam <jwkvam@gmail.com>
2018-09-16 14:25:08 -0700

    update

--- bowtie/tests/test_tags.py
+++ bowtie/tests/test_tags.py
@@ -9,13 +9,15 @@ def test_markdown():
     next_uuid = Markdown._NEXT_UUID
     mark = Markdown()
     assert mark._instantiate == (
+        "<AntProgress socket={{socket}} uuid={{'{prev_uuid}'}}>"
         "<Markdown initial={{''}} "
-        "socket={{socket}} uuid={{'{next_uuid}'}} />"
-    ).format(next_uuid=next_uuid + 1)
+        "socket={{socket}} uuid={{'{next_uuid}'}} /></AntProgress>"
+    ).format(prev_uuid=next_uuid + 1, next_uuid=next_uuid + 2)
 
     next_uuid = Markdown._NEXT_UUID
     mark = Markdown(initial='#hi\n##hello')
     assert mark._instantiate == (
+        "<AntProgress socket={{socket}} uuid={{'{prev_uuid}'}}>"
         "<Markdown initial={{'<h1>hi</h1>\\n<h2>hello</h2>'}} "
-        "socket={{socket}} uuid={{'{next_uuid}'}} />"
-    ).format(next_uuid=next_uuid + 1)
+        "socket={{socket}} uuid={{'{next_uuid}'}} /></AntProgress>"
+    ).format(prev_uuid=next_uuid + 1, next_uuid=next_uuid + 2)
