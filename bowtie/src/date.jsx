f99e99d8059679304a6d8057ad0178866b04ba59
Jacques Kvam <jwkvam@gmail.com>
2018-09-21 23:28:53 -0700

    fix getting date values and restoring them on refresh (#242)
    * hotfix dates
    
    * formatting
    
    * need moment to restore date state :( long unnoticed bug

--- bowtie/src/date.jsx
+++ bowtie/src/date.jsx
@@ -4,6 +4,7 @@ import { DatePicker, LocaleProvider } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 const { MonthPicker, RangePicker } = DatePicker;
 import { storeState } from './utils';
+import moment from 'moment';
 
 var msgpack = require('msgpack-lite');
 
@@ -12,15 +13,21 @@ export default class PickDates extends React.Component {
         super(props);
         var local = sessionStorage.getItem(this.props.uuid);
         if (local === null) {
-            this.state = {value: null};
+            this.state = {value: null, datestring: null};
         } else {
-            this.state = JSON.parse(local);
+            local = JSON.parse(local);
+            if (Array.isArray(local.value)) {
+                local.value = local.value.map(x => moment(x));
+            } else {
+                local.value = moment(local.value);
+            }
+            this.state = local;
         }
     }
 
-    handleChange = (moment, ds) => {
-        this.setState({value: moment});
-        storeState(this.props.uuid, this.state, {value: moment});
+    handleChange = (mom, ds) => {
+        this.setState({value: mom, datestring: ds});
+        storeState(this.props.uuid, this.state, {value: mom, datestring: ds});
         this.props.socket.emit(this.props.uuid + '#change', msgpack.encode(ds));
     }
 
@@ -31,7 +38,7 @@ export default class PickDates extends React.Component {
     }
 
     getValue = (data, fn) => {
-        fn(msgpack.encode(this.state.value));
+        fn(msgpack.encode(this.state.datestring));
     }
 
     render () {
