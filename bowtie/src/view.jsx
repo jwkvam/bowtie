945519dd42b01ce2ef6f37e8ae1fc0fc0d95eea9
Jacques Kvam <jwkvam@gmail.com>
2018-10-02 11:40:40 -0700

    detemplate view

--- bowtie/src/view.jsx
+++ bowtie/src/view.jsx
@@ -1,14 +1,12 @@
 import 'normalize.css';
 import React from 'react';
 import PropTypes from 'prop-types';
-import {render} from 'react-dom';
-import {message} from 'antd';
 
-import { components, socket } from './components';
+import { components } from './components';
 import { str2ints } from './utils';
 
 
-export class View{{ uuid }} extends React.Component {
+export class View extends React.Component {
 
     constructor(props) {
         super(props);
@@ -24,7 +22,7 @@ export class View{{ uuid }} extends React.Component {
     }
 
     render() {
-        var widgets = []
+        var widgets = [];
         const controls = this.state.controllers.map(index =>
             <div key={index.toString()}>{components[index]}</div>
         );
@@ -33,39 +31,40 @@ export class View{{ uuid }} extends React.Component {
                 const comps = this.state.spans[key].map((number) =>
                     <div key={number.toString}>{components[number]}</div>
                 );
-                const rowcols = str2ints(key)
+                const rowcols = str2ints(key);
                 widgets.push(
-                    <div key={key} style={{ '{{' }}
-                            gridColumn: `${ rowcols[1] + this.state.sidebar } / ${ rowcols[3] + this.state.sidebar }`,
-                            gridRow: `${rowcols[0]} / ${rowcols[2]}`,
-                            position: 'relative'
-                            {{ '}}' }}>
+                    <div key={key} style={{
+                        gridColumn: `${ rowcols[1] + this.state.sidebar } / ${ rowcols[3] + this.state.sidebar }`,
+                        gridRow: `${rowcols[0]} / ${rowcols[2]}`,
+                        position: 'relative'
+                    }}>
                         {comps}
                     </div>
-                )
+                );
             }
         }
 
         return (
-            <div style={{ '{{' }}display: 'grid',
-                    gridTemplateColumns: this.state.columns,
-                    gridTemplateRows: this.state.rows,
-                    gridColumnGap: this.state.column_gap,
-                    gridRowGap: this.state.row_gap,
-                    width: '100%', height: '100%',
-                    minHeight: '100vh', maxHeight: '100%',
-                    minWidth: '100vw', maxWidth: '100%'{{ '}}' }}>
+            <div style={{display: 'grid',
+                gridTemplateColumns: this.state.columns,
+                gridTemplateRows: this.state.rows,
+                gridColumnGap: this.state.column_gap,
+                gridRowGap: this.state.row_gap,
+                width: '100%', height: '100%',
+                minHeight: '100vh', maxHeight: '100%',
+                minWidth: '100vw', maxWidth: '100%'}}>
 
                 {this.state.sidebar &&
-                    <div style={{ '{{' }}padding: '7px', backgroundColor: '{{background_color}}',
-                            gridColumn: '1 / 2',
-                            gridRow: '1 / -1'
-                            {{ '}}' }}>
+                    <div style={{padding: '7px',
+                        backgroundColor: `${this.props.background_color}`,
+                        gridColumn: '1 / 2',
+                        gridRow: '1 / -1'
+                    }}>
 
                         {controls}
                     </div>
                 }
-                
+
                 {widgets}
 
             </div>
@@ -73,8 +72,9 @@ export class View{{ uuid }} extends React.Component {
     }
 }
 
-View{{ uuid }}.propTypes = {
+View.propTypes = {
     uuid: PropTypes.string.isRequired,
+    background_color: PropTypes.string.isRequired,
     spans: PropTypes.object.isRequired,
     controllers: PropTypes.arrayOf(PropTypes.number).isRequired,
     columns: PropTypes.string.isRequired,
@@ -82,4 +82,4 @@ View{{ uuid }}.propTypes = {
     column_gap: PropTypes.string.isRequired,
     row_gap: PropTypes.string.isRequired,
     sidebar: PropTypes.bool.isRequired,
-}
+};
